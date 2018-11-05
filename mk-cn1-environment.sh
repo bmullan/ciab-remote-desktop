7d4ded547a2c508bc265862e4129a199cd3911ca
brian mullan <bmullan.mail@gmail.com>
2018-11-05 03:25:09 -0500

    Install required packages for CIAB container CN1
    This script installs all necessary software into the initial CIAB Desktop container named CN1.   This includes the Ubuntu-Mate desktop environment (commented examples to install XFCE4 or Budgie are included also), xrdp for RDP server support of RDP connections, initial admin/sudo user account using the Installer's User ID etc.

--- mk-cn1-environment.sh
+++ mk-cn1-environment.sh
@@ -36,6 +36,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
+# All Guacamole code is Apache 2 Copyright as described on the Guacamole website.
 #========================================================================================================================
 
 #
