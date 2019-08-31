2d24347cd512c8524f81aa30193d4b77cf6a8ba5
brian mullan <bmullan.mail@gmail.com>
2019-08-31 12:25:35 -0400

    Script to install Guacamole, NGINX, Tomcat in ciab-guac container

--- setup-guacamole.sh
+++ setup-guacamole.sh
@@ -21,7 +21,7 @@ clear
 #=================================================================================
 # Copyright (c) 2016-2018 Brian Mullan
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
